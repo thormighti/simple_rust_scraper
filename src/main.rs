049fabfc297e0eaaeed286508bbd114c1c83d699
thormighti <peterabeshi4@gmail.com>
2022-09-15 03:23:07 +0100

    some more scraping

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,21 @@
+// use scraper::Html;
+
 mod scapper;
 
-
-fn main()  {
-
-    scapper("https://twitter.com/home","div.DraftEditor-editorContainer>div");
-
-  
+fn main() {
+    scapper("https://fedoramagazine.org/", "div.magazine-about>p>a");
 }
 
-
-fn scapper(url:&str, the_selector: &str){
-
+fn scapper(url: &str, the_selector: &str) {
     let response = reqwest::blocking::get(url).unwrap().text().unwrap();
     let document = scraper::Html::parse_document(&response);
-    
+    // let fragment = Html::parse_fragment(the_selector); tried selecting attribute
+
     let selector = scraper::Selector::parse(the_selector).unwrap();
 
-    let title = document.select(&selector).map(|x| x.inner_html());
+    // let input = fragment.select(&selector).next().unwrap(); same here
 
-    title.zip(1..100).for_each(|(item, number)| println!("{}, {} ", number, item));
+    let result = document.select(&selector).map(|x| x.inner_html()); // concern with the inner html or html depending
 
-
+    result.for_each(|items| println!("{} ", items));
 }
-
-
